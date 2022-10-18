7d724a428d1537af0b67bebe5ad66bfafc08e814
Thyrrion <kevinschueler2000@gmail.com>
2022-10-18 13:24:27 +0200

    Unittests mit Jest

---
+++ src/mapj.js
@@ -0,0 +1,25 @@
+export default class MapUser {
+    map(user) {
+      return {
+        first_name: user.firstName,
+        id: user.id,
+        last_name: user.lastName,
+        user_name: this.mapUsername(user.firstName, user.lastName),
+      };
+    }
+    mapUsername(username) {
+      return username.substring(0, 8);
+    }
+    myException() {
+      throw new Error('whoops');
+    }
+  
+    myAsync() {
+      return new Promise((res, rej) => {
+        setTimeout(() => {
+          res('whoooho');
+        }, 1000);
+      });
+    }
+  }
+  
